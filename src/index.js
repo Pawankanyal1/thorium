cf5e22816b57e86030b46f277c7f3f703bf1d9b5
pawankanyal1 <pawaneshkanyal1@gmail.com>
2022-02-28 01:15:03 +0530

    solution 24-02-22

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const app = express();
+const { default : mongoose} = require('mongoose')
+
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://Pawankanyal1:6Ntt9WQRMlMCVLPc@cluster0.g2udp.mongodb.net/pawankanyal1-DB" , { useNewUrlParser: true})
+
+.then( () => console.log("mongoDb is connected"))
+.catch ( err => console.log(err))
+
+
+
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
