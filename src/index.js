1761370e11fe572df19c8b8ffb982990ea9e4137
pawankanyal1 <pawaneshkanyal1@gmail.com>
2022-03-09 21:31:00 +0530

    9 March Assignment Complete

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Pawankanyal1:6Ntt9WQRMlMCVLPc@cluster0.g2udp.mongodb.net/pawankanyal5-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
