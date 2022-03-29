c2be76ef331574ddc0f04504651569b25b537245
pawankanyal1 <pawaneshkanyal1@gmail.com>
2022-03-30 02:01:02 +0530

    Team 20 solution

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const route = require("../src/router/routes.js");
-const { default: mongoose } = require('mongoose');
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
+mongoose
+  .connect(
+    "mongodb+srv://Pawankanyal1:6Ntt9WQRMlMCVLPc@cluster0.g2udp.mongodb.net/Project3BookManagement-DB",
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
 
-mongoose.connect("mongodb+srv://group13:UEEqzwKeluhyT2uM@cluster0.hkvjs.mongodb.net/Sanchita_project3?retryWrites=true&w=majority" , {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-
-app.use('/', route);
-
+app.use("/", route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
